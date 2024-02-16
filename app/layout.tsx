e080be4bedc3077c73e27df2b3b312a66b18b1ac
arlettearenas <arlettearenas29@gmail.com>
2024-02-15 23:40:55 -0600

    no puedo marta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import { inter } from '@/app/ui/fonts';
+import { inter, lusi } from '@/app/ui/fonts';
 import '@/app/ui/global.css';
- 
+
 export default function RootLayout({
   children,
 }: {
@@ -8,6 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <body className={`${lusi.className} antialiased`}>{children}</body>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
